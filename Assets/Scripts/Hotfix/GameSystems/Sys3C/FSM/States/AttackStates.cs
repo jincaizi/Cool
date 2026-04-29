539961963d55777d4835e6e0c6e1cffcb552dce2
jincaizi <zhangjincai@aliyun.com>
2026-04-29 12:24:51 +0800

    feat(3c): add AttackFSM states (AttackIdle, Attack1/2, SkillQ/R)

---
+++ Assets/Scripts/Hotfix/GameSystems/Sys3C/FSM/States/AttackStates.cs
@@ -0,0 +1,62 @@
+namespace Hotfix.GameSystems.Sys3C.FSM.States
+{
+    /// <summary>
+    /// AttackIdle 状态
+    /// </summary>
+    public class AttackIdleState : IState
+    {
+        public string StateName => "AttackIdle";
+
+        public void Enter() { }
+        public void Exit() { }
+        public void Update(float deltaTime) { }
+    }
+
+    /// <summary>
+    /// Attack1 状态
+    /// </summary>
+    public class Attack1State : IState
+    {
+        public string StateName => "Attack1";
+
+        public void Enter() { }
+        public void Exit() { }
+        public void Update(float deltaTime) { }
+    }
+
+    /// <summary>
+    /// Attack2 状态
+    /// </summary>
+    public class Attack2State : IState
+    {
+        public string StateName => "Attack2";
+
+        public void Enter() { }
+        public void Exit() { }
+        public void Update(float deltaTime) { }
+    }
+
+    /// <summary>
+    /// SkillQ 状态（突刺）
+    /// </summary>
+    public class SkillQState : IState
+    {
+        public string StateName => "SkillQ";
+
+        public void Enter() { }
+        public void Exit() { }
+        public void Update(float deltaTime) { }
+    }
+
+    /// <summary>
+    /// SkillR 状态
+    /// </summary>
+    public class SkillRState : IState
+    {
+        public string StateName => "SkillR";
+
+        public void Enter() { }
+        public void Exit() { }
+        public void Update(float deltaTime) { }
+    }
+}
