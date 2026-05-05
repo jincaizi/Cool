23d1467611563f45b033c1f4185b6a95866a12f5
jincaizi <zhangjincai@aliyun.com>
2026-05-06 03:24:43 +0800

    feat(monster): add MonsterMovement with NavMeshAgent

---
+++ Assets/Scripts/Hotfix/GameSystems/Monster/MonsterMovement.cs
@@ -0,0 +1,64 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace Hotfix.GameSystems.Monster
+{
+    public class MonsterMovement
+    {
+        private readonly NavMeshAgent _agent;
+        private readonly Transform _self;
+
+        public bool HasReachedDestination =>
+            !_agent.pathPending && _agent.remainingDistance <= _agent.stoppingDistance + 0.1f;
+
+        public MonsterMovement(NavMeshAgent agent, Transform self, MonsterConfig config)
+        {
+            _agent = agent;
+            _self = self;
+            _agent.speed = config.MoveSpeed;
+            _agent.stoppingDistance = config.AttackRange * 0.8f;
+        }
+
+        public void Stop()
+        {
+            _agent.isStopped = true;
+            _agent.velocity = Vector3.zero;
+        }
+
+        public void Resume()
+        {
+            _agent.isStopped = false;
+        }
+
+        public void Chase(Transform target)
+        {
+            _agent.isStopped = false;
+            _agent.SetDestination(target.position);
+        }
+
+        public void PatrolTo(Vector3 point)
+        {
+            _agent.isStopped = false;
+            _agent.SetDestination(point);
+        }
+
+        public void ReturnToSpawn(Vector3 spawnPoint)
+        {
+            _agent.isStopped = false;
+            _agent.SetDestination(spawnPoint);
+        }
+
+        public void LookAt(Vector3 target)
+        {
+            Vector3 dir = target - _self.position;
+            dir.y = 0;
+            if (dir.sqrMagnitude > 0.01f)
+            {
+                _self.rotation = Quaternion.Slerp(
+                    _self.rotation,
+                    Quaternion.LookRotation(dir),
+                    10f * Time.deltaTime);
+            }
+        }
+    }
+}
