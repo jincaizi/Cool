5e2951e256d4fe521ee7d92cb1574d6cf89458af
jincaizi <zhangjincai@aliyun.com>
2026-05-15 17:07:36 +0800

    feat: add NameplateSettings ScriptableObject

---
+++ Assets/Scripts/Hotfix/GameSystems/Nameplate/NameplateSettings.cs
@@ -0,0 +1,20 @@
+using TMPro;
+using UnityEngine;
+
+namespace Hotfix.GameSystems.Nameplate
+{
+    [CreateAssetMenu(menuName = "Display/NameplateSettings", fileName = "NameplateSettings")]
+    public class NameplateSettings : ScriptableObject
+    {
+        public TMP_FontAsset Font;
+        public Material FontMaterial;
+        public float FontSize = 18f;
+        public Color DefaultColor = Color.white;
+        public float OutlineWidth = 0.15f;
+        public Color OutlineColor = Color.black;
+        public float VerticalOffset = 2.5f;
+        public float CullDistance = 50f;
+        public float FadeStartDistance = 30f;
+        public Vector2 IconSize = new(20, 20);
+    }
+}
