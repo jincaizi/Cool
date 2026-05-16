8eecaad5a1851f595a26d303daecbfeeb5f3a7f5
jincaizi <zhangjincai@aliyun.com>
2026-05-17 04:33:23 +0800

    feat: integrate selection ring into CharacterAttackHandler

--- Assets/Scripts/Hotfix/GameSystems/Sys3C/CharacterAttackHandler.cs
+++ Assets/Scripts/Hotfix/GameSystems/Sys3C/CharacterAttackHandler.cs
@@ -1,12 +1,19 @@
+using System;
+using System.Collections.Generic;
 using UnityEngine;
 using Hotfix.GameSystems.Sys3C.Core.Combat;
+using Hotfix.GameSystems.Monster;
 
 namespace Hotfix.GameSystems.Sys3C
 {
     public class CharacterAttackHandler : MonoBehaviour
     {
         [SerializeField] private LayerMask _targetMask = -1;
+        [SerializeField] private SelectionRing _selectionRing;
+
         private IWeapon _currentWeapon;
+        private ITargetable _currentTarget;
+        private Action _onTargetDeath;
 
         private void Start()
         {
@@ -20,7 +27,41 @@ namespace Hotfix.GameSystems.Sys3C
         public void OnAttackActivated()
         {
             if (_currentWeapon == null || !_currentWeapon.CanAttack()) return;
-            _currentWeapon.Attack(transform.forward, _targetMask);
+            var hits = _currentWeapon.Attack(transform.forward, _targetMask);
+            if (hits.Count > 0)
+                SelectTarget(hits[0]);
+        }
+
+        public void SelectTarget(IDamageable target)
+        {
+            if (!(target is ITargetable targetable) || targetable == _currentTarget) return;
+            if (_selectionRing == null) return;
+
+            // Unsubscribe old target's death event
+            if (_currentTarget != null && _onTargetDeath != null)
+            {
+                _currentTarget.OnDeath -= _onTargetDeath;
+                _onTargetDeath = null;
+            }
+
+            _selectionRing.Detach();
+
+            float yOffset = -0.9f;
+            if (target is MonsterEntity monster && monster.Config != null)
+                yOffset = monster.Config.RingYOffset;
+
+            _currentTarget = targetable;
+            _onTargetDeath = () =>
+            {
+                if (_currentTarget != null)
+                    _currentTarget.OnDeath -= _onTargetDeath;
+                _selectionRing.Detach();
+                _currentTarget = null;
+                _onTargetDeath = null;
+            };
+            _currentTarget.OnDeath += _onTargetDeath;
+
+            _selectionRing.AttachTo(target.Transform, yOffset);
         }
     }
 }
