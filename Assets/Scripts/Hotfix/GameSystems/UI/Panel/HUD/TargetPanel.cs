51f8a528127a060053f174b936e4224611342d73
jincaizi <zhangjincai@aliyun.com>
2026-05-22 18:46:38 +0800

    refactor: replace TargetPanel Slider with Image.fillAmount, add MP bar and buff icons

--- Assets/Scripts/Hotfix/GameSystems/UI/Panel/HUD/TargetPanel.cs
+++ Assets/Scripts/Hotfix/GameSystems/UI/Panel/HUD/TargetPanel.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using Hotfix.GameSystems.Sys3C.Core.Combat;
 using TMPro;
 using UnityEngine;
@@ -14,16 +15,23 @@ namespace Hotfix.GameSystems.UI
         [SerializeField] private Image _portrait;
         [SerializeField] private TMP_Text _nameText;
         [SerializeField] private TMP_Text _levelText;
-        [SerializeField] private Slider _hpSlider;
-        [SerializeField] private TMP_Text _hpText;
+        [SerializeField] private Image _hpFill;
+        [SerializeField] private TMP_Text _hpOverlay;
+        [SerializeField] private Image _mpFill;
+        [SerializeField] private TMP_Text _mpOverlay;
+        [SerializeField] private Transform _buffContainer;
+        [SerializeField] private GameObject _buffIconPrefab;
         [SerializeField] private GameObject _contentRoot;
 
         private ITargetable _currentTarget;
+        private ITargetStatsProvider _targetStats;
+        private readonly List<GameObject> _activeBuffIcons = new();
 
         public void Bind(ITargetable target)
         {
             Clear();
             _currentTarget = target;
+            _targetStats = target as ITargetStatsProvider;
 
             if (_nameText != null) _nameText.text = target.DisplayName;
             if (_levelText != null) _levelText.text = $"Lv.{target.Level}";
@@ -32,6 +40,14 @@ namespace Hotfix.GameSystems.UI
             UpdateHP(target.HPPercent, target.CurrentHP, target.MaxHP);
             target.OnHPChanged += UpdateHP;
             target.OnDeath += OnTargetDeath;
+
+            if (_targetStats != null)
+            {
+                UpdateMP(_targetStats.MPPercent, _targetStats.CurrentMP, _targetStats.MaxMP);
+                UpdateBuffs(_targetStats.ActiveBuffs);
+                _targetStats.OnMPChanged += UpdateMP;
+                _targetStats.OnBuffsChanged += UpdateBuffs;
+            }
 
             if (_contentRoot != null) _contentRoot.SetActive(true);
         }
@@ -44,14 +60,60 @@ namespace Hotfix.GameSystems.UI
                 _currentTarget.OnDeath -= OnTargetDeath;
                 _currentTarget = null;
             }
-
+            if (_targetStats != null)
+            {
+                _targetStats.OnMPChanged -= UpdateMP;
+                _targetStats.OnBuffsChanged -= UpdateBuffs;
+                _targetStats = null;
+            }
+            ClearBuffIcons();
             if (_contentRoot != null) _contentRoot.SetActive(false);
         }
 
         private void UpdateHP(float percent, int current, int max)
         {
-            if (_hpSlider != null) _hpSlider.value = percent;
-            if (_hpText != null) _hpText.text = $"{current}/{max}";
+            if (_hpFill != null) _hpFill.fillAmount = percent;
+            if (_hpOverlay != null) _hpOverlay.text = $"{current}/{max}";
+        }
+
+        private void UpdateMP(float percent, int current, int max)
+        {
+            if (_mpFill != null) _mpFill.fillAmount = percent;
+            if (_mpOverlay != null) _mpOverlay.text = $"{current}/{max}";
+        }
+
+        private void UpdateBuffs(BuffInfo[] buffs)
+        {
+            ClearBuffIcons();
+            if (buffs == null || buffs.Length == 0 || _buffContainer == null || _buffIconPrefab == null) return;
+
+            foreach (var buff in buffs)
+            {
+                var go = GetOrCreateBuffIcon();
+                go.SetActive(true);
+                var icon = go.GetComponent<BuffIcon>();
+                if (icon == null) icon = go.AddComponent<BuffIcon>();
+                icon.SetBuff(buff);
+            }
+        }
+
+        private GameObject GetOrCreateBuffIcon()
+        {
+            foreach (var go in _activeBuffIcons)
+            {
+                if (!go.activeSelf) return go;
+            }
+            var newGo = Instantiate(_buffIconPrefab, _buffContainer);
+            _activeBuffIcons.Add(newGo);
+            return newGo;
+        }
+
+        private void ClearBuffIcons()
+        {
+            foreach (var go in _activeBuffIcons)
+            {
+                if (go != null) go.SetActive(false);
+            }
         }
 
         private void OnTargetDeath()
