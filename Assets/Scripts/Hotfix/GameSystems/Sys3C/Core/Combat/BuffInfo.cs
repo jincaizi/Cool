645a0def2f84304453d9f2519f86870c3e6eb0f5
jincaizi <zhangjincai@aliyun.com>
2026-05-22 18:39:25 +0800

    feat: add BuffInfo struct

---
+++ Assets/Scripts/Hotfix/GameSystems/Sys3C/Core/Combat/BuffInfo.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+namespace Hotfix.GameSystems.Sys3C.Core.Combat
+{
+    public struct BuffInfo
+    {
+        public string Id;
+        public Sprite Icon;
+        public float RemainingTime;
+        public float Duration;
+        public bool IsDebuff;
+    }
+}
